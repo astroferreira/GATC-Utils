a1c46e04d6d8cb7a9942e05786605e24ed7a34f1
astroferreira <leonardo.ferreira.furg@gmail.com>
2015-12-10 08:29:58 -0800

    Add base documentation to master file

--- gatcutils.py
+++ gatcutils.py
@@ -1,13 +1,36 @@
+#!/usr/bin/env python
+"""
+Module with utility functions for the GATC @ FURG.
+
+This module brings together a set of utility functions for astrophysics
+that are developed as a subproduct of the research made by the Grupo de
+Astrofísica Teórica e Computacional @ FURG. All functions presented here
+are not directly related to each other and are developed by several authors
+within the research group.
+
+"""
+
 import os
-import pyfits as pf
 import numpy as np
-import gc
-import time
+import pyfits as pf
+
+__author__ = "Leonardo Ferreira and Fabricio Ferrari"
+__credits__ = ["GATC Group"]
+__version__ = "0.1"
+__maintainer__ = "Leonardo Ferreira"
+__email__ = "leonardo.ferreira@furg.br"
+__status__ = "Development"
 
 def normalizePSFs(path):
+	"""
+		Normalize all PSFs in given path.
+
+		If, for some reason, you got a folder with a bunch of unormalized
+		PSFs, this function will normalize each one. 
+		
+	"""
 	psfsNames = sorted(os.listdir(path))
 
-	#psfsNames = psfsNames[1999:]
 	for name in psfsNames:
 		print name
 		try:
